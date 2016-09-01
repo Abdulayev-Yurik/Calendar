79808066a19a54e108a4e42e08aa752bad039793
Yurik Abdulayev <yurs11k89@gmail.com>
2016-09-01 19:53:47 +0300

    add printColor, parse date and draw calendar view

--- src/RunCalendar.java
+++ src/RunCalendar.java
@@ -1,3 +1,6 @@
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.temporal.TemporalField;
 import java.util.Scanner;
 
 /**
@@ -14,11 +17,43 @@ public class RunCalendar {
         String month = scanner.nextLine();
 
         if (month.isEmpty()) {
-
+            parseDate(LocalDate.now().getMonth().getValue());
         } else {
-
+            parseDate(Integer.parseInt(month));
         }
     }
 
+    private static void parseDate(int month) {
+        if (month <= 0 || month > 12) {
+            System.out.println("incorrect");
+            return;
+        }
+        LocalDate thisDate = LocalDate.of(LocalDate.now().getYear(), month, 1);
+        int firstDayInWeek = thisDate.getDayOfWeek().getValue();
+        int monthLength = thisDate.getMonth().length(thisDate.isLeapYear());
+        System.out.println("count = " + monthLength + ", first day from month in week = " + firstDayInWeek);
+        generateView(monthLength, firstDayInWeek);
+    }
 
+    private static void generateView(int monthLength, int firstDayInWeek) {
+        System.out.println("   П   В   С   Ч   П   " + colorPrint("С   Н"));
+        int counter  = 1;
+        for (int i = 1; i <= 6; i++) {
+            for (int j = 1; j <= 7; j++) {
+                if(i == 1 && j >= firstDayInWeek) {
+                    if (j >= 6) System.out.printf(ANSI_RED + "%4d" + ANSI_RESET, counter++);
+                    else System.out.printf("%4d", counter++);
+                }else if (i > 1 && monthLength >= counter) {
+                    if (j >= 6) System.out.printf(ANSI_RED + "%4d" + ANSI_RESET, counter++);
+                    else System.out.printf("%4d", counter++);
+                }else
+                    System.out.print("    ");
+            }
+            System.out.println();
+        }
+    }
+
+    private static String colorPrint(String text){
+        return ANSI_RED + text + ANSI_RESET;
+    }
 }
