c2ce03cb33b295f023aa7fac2d6778d0fb1ff6f5
Yurik Abdulayev <yurs11k89@gmail.com>
2016-09-01 19:51:46 +0300

    scan index month

--- src/RunCalendar.java
+++ src/RunCalendar.java
@@ -1,9 +1,24 @@
+import java.util.Scanner;
+
 /**
  * Created by yurik on 01.09.16.
  */
 public class RunCalendar {
 
-    public static void main(String[] args) {
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_RED = "\u001B[31m";
 
+    public static void main(String[] args) {
+        System.out.print("Please enter month: ");
+        Scanner scanner = new Scanner(System.in);
+        String month = scanner.nextLine();
+
+        if (month.isEmpty()) {
+
+        } else {
+
+        }
     }
+
+
 }
