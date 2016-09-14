67c70b42566b16f9e2429c0d01af7946885f9c1d
Yurik Abdulayev <yurs11k89@gmail.com>
2016-09-14 23:30:53 +0300

    refactoring

--- src/calendar/PrintToConsole.java
+++ src/calendar/PrintToConsole.java
@@ -3,8 +3,9 @@ package calendar;
 import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.time.format.TextStyle;
-import java.util.Formatter;
 import java.util.Locale;
+
+import static java.lang.String.format;
 
 
 /**
@@ -31,10 +32,11 @@ class PrintToConsole {
     }
 
     private static void generateView(int monthLength, int firstDayInWeek, int day) {
-        System.out.println(printDaysName());
-        for (int week = 1; week <= MAX_WEEKS; week++) {
+        for (int week = 0; week <= MAX_WEEKS; week++) {
             for (int dayInWeek = 1; dayInWeek <= DAYS_IN_WEEK; dayInWeek++) {
-                if (counter <= monthLength)
+                if (week == 0)
+                    printDay(dayInWeek, day, printDaysName(dayInWeek));
+                else if (counter <= monthLength)
                     actionDay(firstDayInWeek, dayInWeek, week, day);
                 else
                     break;
@@ -45,34 +47,27 @@ class PrintToConsole {
 
     private static void actionDay(int firstDayInWeek, int dayInWeek, int week, int day) {
         if (week == 1) {
-            if (dayInWeek >= firstDayInWeek)
-                printDay(dayInWeek, day);
-            else
-                System.out.print("    ");
-        } else {
-            printDay(dayInWeek, day);
+            if (dayInWeek >= firstDayInWeek) {
+                printDay(dayInWeek, day, Integer.toString(counter));
+                counter++;
+            }else
+                System.out.print(format("%4s", ""));
+        } else{
+            printDay(dayInWeek, day, Integer.toString(counter));
+            counter++;
         }
     }
 
-    private static String printDaysName() {
-        String daysName = "";
-        for (int i = 1; i <= DAYS_IN_WEEK; i++) {
-            String day = DayOfWeek.of(i).getDisplayName(TextStyle.SHORT, Locale.getDefault()).toUpperCase();
-            if (i == DayOfWeek.SATURDAY.getValue() || i == DayOfWeek.SUNDAY.getValue())
-                daysName += new Formatter().format(ANSI_RED + "%4s" + ANSI_RESET, day);
-            else
-                daysName += new Formatter().format("%4s", day);
-        }
-        return daysName;
+    private static String printDaysName(int dayNumber) {
+        return DayOfWeek.of(dayNumber).getDisplayName(TextStyle.SHORT, Locale.getDefault()).toUpperCase();
     }
 
-    private static void printDay(int dayInWeek, int day) {
+    private static void printDay(int dayInWeek, int day, String value) {
         if (counter == day)
-            System.out.printf(ANSI_CYAN + "%4d" + ANSI_RESET, counter);
-        else if (dayInWeek >= DayOfWeek.SATURDAY.getValue())
-            System.out.printf(ANSI_RED + "%4d" + ANSI_RESET, counter);
+            System.out.print(format(ANSI_CYAN + "%4s" + ANSI_RESET, value));
+        else if (dayInWeek == DayOfWeek.SATURDAY.getValue() || dayInWeek == DayOfWeek.SUNDAY.getValue())
+            System.out.print(format(ANSI_RED + "%4s" + ANSI_RESET, value));
         else
-            System.out.printf("%4d", counter);
-        counter++;
+            System.out.print(format("%4s", value));
     }
 }
