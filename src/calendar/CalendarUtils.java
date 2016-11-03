23afb806e0d026d2c77e4aa56b26c06d9ab6bd01
Yurik Abdulayev <yuriia@softwareplanet.com>
2016-11-03 11:21:42 +0200

    Add calendar utils

--- src/calendar/CalendarUtils.java
+++ src/calendar/CalendarUtils.java
@@ -3,6 +3,7 @@ package calendar;
 import java.util.Arrays;
 import java.util.List;
 
+import static calendar.PrintToConsole.ANSI_CYAN;
 import static calendar.PrintToConsole.ANSI_RED;
 import static calendar.PrintToConsole.ANSI_RESET;
 
@@ -11,12 +12,19 @@ import static calendar.PrintToConsole.ANSI_RESET;
  */
 public class CalendarUtils {
 
+    static final String TYPICAL_STRING_FORMAT = "%4s";
+
+
     public static boolean isWeekend(int dayOfWeek, List weekends){
         return weekends.contains(dayOfWeek);
     }
 
     public static String toWeekendColor(String value){
         return ANSI_RED + value + ANSI_RESET;
+    }
+
+    public static String toThisDayColor(String value){
+        return ANSI_CYAN + value + ANSI_RESET;
     }
 
 
